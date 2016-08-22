302ed611d27e9d3335f2d6cf94ad86b7ac1e5d3a
Francisco Rojo <frojo@andrew.cmu.edu>
2016-08-22 10:36:08 -0400

    adding cmake stuff and getting it to compile on my computer

--- scratch/gltest/gltest.cpp
+++ scratch/gltest/gltest.cpp
@@ -18,6 +18,44 @@ int main()
 		return -1;
 	}
 
+	// TODO: Should probobaly not multisample if we're trying to do a
+	// pixel art game
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Open a window
+	window = glfwCreateWindow(1024, 768, "Test", NULL, NULL);
+	if (!window) {
+		fprintf(stderr, "Failed to opwn GLFW window. If you have Intel GPU, they are not 3.3 compatible");
+	//	getchar();
+	//	glfwTerminate();
+		return -1;
+	}
+
+	GLuint VertexArrayID;
+	glGenVertexArrays1, &
+
+	glfwMakeContextCurrent(window);
+	glewExperimental=true;
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to Init GLEW\n");
+		return -1;
+	}
+
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	do {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+
+
+glfwTerm:
+	glfwTerminate();
 
 	return 0;
 }
